7a013ed5d56e087b53516326a234328210d18e40
梦泽 陈 <1398947448@qq.com>
2024-11-01 22:15:00 +0800

    config done

--- src/services/config.py
+++ src/services/config.py
@@ -1,25 +1,51 @@
 """_summary_
-    TODO 配置文件交互
+    配置文件交互
 """
 import json
-folder="config/"
-name="user.json"
+from helper import *
 
+file_path="config/user.json"
+
+jval_t = Union[dict,list,str,int,float,bool,None]
+jkey_t = Union[str,int]
+@singleton
+@dataclass
 class Config:
-    def __init__(self, cfg:dict):
-        self.cfg:dict = cfg # cfg 是 Python dict 而 Config 是自定义类型
+    __slots__ = ["pth","cfg"]
+    cfg:dict
+    pth:str
+    def __init__(self, path:str) -> None:
+        self.cfg = {}
+        self.pth = ""
+        self.load(path)
         
+    def load(self,path:str)->None: # json -> cfg:dict 将文件中的内容加载到实例中
+        with open(path, "r", encoding="utf-8") as f:
+            self.cfg = json.load(f)
+            self.pth = path
     
+    def save(self,path:str)->None: # cfg:dict -> json 将实例中的更改保存到文件中
+        with open(path, "w", encoding="utf-8") as f:
+            json.dump(self.cfg, f, indent=4, ensure_ascii=False)
+            self.pth = path
     
-def get_config()->Config:
-    with open(folder+name, "r", encoding="utf-8") as f:
-        cfg = json.load(f)
-    return Config(cfg)
-def set_config(config:Config)->None:
-    with open(folder+name, "w", encoding="utf-8") as f:
-        json.dump(config.dict, f, indent=4, ensure_ascii=False)
+    def get(self,*keys)->jval_t: # 通过keys找到值 这个值你可以直接使用和更改 更改会同步到这个class的实例
+        cur=self.cfg
+        for key in keys:
+            cur = cur[key]
+        return cur
+    
+    def keys(self,*keys)->tuple[jkey_t]: # 通过keys找到值 返回这个值的所有key
+        cur = self.get(*keys)
+        if type(cur) == dict:
+            return tuple(cur.keys())
+        elif type(cur) == list:
+            return tuple(range(len(cur)))
+        else:
+            return []
 
-
-    
-    
-set_config(get_config())
+if __name__ == "__main__":
+    c1=Config(file_path)
+    c2=Config(file_path)
+    # dprint(c1,c2)
+    print(c1==c2 , c1 is c2 , len({c1:1,c2:1}))
--- src/services/helper.py
+++ src/services/helper.py
@@ -1,24 +1,91 @@
-import time
-def dataclass(cls): # 装饰器，给类加上__repr__,__str__,__eq__,__hash__方法
+from typing import Any, Optional, Callable, Union
+
+def singleton(cls): # 类装饰器，给类加上单例模式    ！！必须放在其他类装饰器之上（之外） 因为他会把类退化成函数
+    instances = {}
+    def get_instance(*args, **kwargs):
+        if cls not in instances:
+            instances[cls] = cls(*args, **kwargs)
+        return instances[cls]
+    return get_instance
+
+from textwrap import shorten
+def dataclass(cls): # 类装饰器，给类加上__repr__,__str__,__eq__,__hash__方法
     def __str__(self)->str:
-        return f"type:{self.__class__.__name__}"
+        ret:str = f"class:{self.__class__.__name__}\t"
+        if hasattr(self,"__slots__"):
+            for attr in self.__slots__:
+                ret += f"\t{attr} = "+ shorten(f"{getattr(self,attr)}",50)
+        elif hasattr(self,"__dict__"):
+            for k,v in self.__dict__.items():
+                ret += f"\t{k} : "+ shorten(f"{v}",50)
+        return ret
     cls.__str__ = __str__
     def __repr__(self)->str:
-        return f"type:{self.__class__.__name__}\tattrs:{self.__dict__}"
+        ret:str = f"class:{self.__class__.__name__}\t"
+        if hasattr(self,"__slots__"):
+            for attr in self.__slots__:
+                ret += f"\t{attr} = "+ shorten(f"{getattr(self,attr)}",200)
+        elif hasattr(self,"__dict__"):
+            for k,v in self.__dict__.items():
+                ret += f"\t{k} : "+ shorten(f"{v}",200)
+        return ret
     cls.__repr__ = __repr__
     def __eq__(self,other)->bool:
-        return self.__dict__ == other.__dict__
+        if hasattr(self,"__slots__"):
+            return all(getattr(self,attr)==getattr(other,attr) for attr in self.__slots__)
+        elif hasattr(self,"__dict__"):
+            return self.__dict__ == other.__dict__
     cls.__eq__ = __eq__
     def __hash__(self)->int:
-        return hash(self.__dict__)
+        if hasattr(self,"__slots__"):
+            return hash([(attr,getattr(self,attr)) for attr in self.__slots__])
+        elif hasattr(self,"__dict__"):
+            return hash(self.__dict__)
     __hash__ = __hash__
     return cls
 
+def noexception(func): # 装饰器，给函数加上try-except
+    def wrapper(*args,**kwargs):
+        try:
+            return func(*args,**kwargs)
+        except Exception as e:
+            print(f"ERROR:{e}")
+    return wrapper
+
+
+import time
 class timeit: # 计时器 计算with timeit():后面的代码块运行时间
     def __enter__(self):
         self.start = time.time()
+        print(f"time start:{self.start}")
         return self
     def __exit__(self, exc_type, exc_val, exc_tb):
-        print(f"E:{exc_type}\t{exc_val}\t{exc_tb}")
-        print(f"T:{time.time()-self.start}")
+        if exc_tb is not None:
+            print(f"ERROR:{exc_type}\t{exc_val}\t{exc_tb}")
+        print(f"time cost:{time.time()-self.start}")
+        
+def timeme(func:Callable)->Callable: # 装饰器，给函数加上计时器
+    def wrapper(*args,**kwargs):
+        start=time.time()
+        ret = func(*args,**kwargs)
+        print(f"{func.__name__}\tcost time:{time.time()-start}")
+        return ret
+    return wrapper
 
+def dprint(*args): # 带类型和详细信息的打印
+    for arg in args:
+        print(f"> id:\t{id(arg)}\n> type:\t{type(arg)}\n> str:\t{arg}\n> repr:\t{repr(arg)}")
+
+if __name__ == "__main__":
+    @singleton
+    @dataclass
+    class A:
+        __slots__ = ["a","b"]
+        a:int
+        b:str
+        def __init__(self,a,b):
+            self.a = a
+            self.b = b
+    a = A(1,2)
+    b = A(1,2)
+    dprint(a , b)
