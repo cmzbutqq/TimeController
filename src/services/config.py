f2fcc12ab6ed6a665abac8244dd39dd9e8d19d86
梦泽 陈 <1398947448@qq.com>
2024-11-01 15:32:58 +0800

    config io working in progress

--- src/services/config.py
+++ src/services/config.py
@@ -2,8 +2,24 @@
     TODO 配置文件交互
 """
 import json
+folder="config/"
+name="user.json"
 
-usr_pth="locker.py"
+class Config:
+    def __init__(self, cfg:dict):
+        self.cfg:dict = cfg # cfg 是 Python dict 而 Config 是自定义类型
+        
+    
+    
+def get_config()->Config:
+    with open(folder+name, "r", encoding="utf-8") as f:
+        cfg = json.load(f)
+    return Config(cfg)
+def set_config(config:Config)->None:
+    with open(folder+name, "w", encoding="utf-8") as f:
+        json.dump(config.dict, f, indent=4, ensure_ascii=False)
 
-with open(usr_pth, "r", encoding="utf-8") as f:
-    print(f.read())
+
+    
+    
+set_config(get_config())
