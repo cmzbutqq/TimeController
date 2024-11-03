5c26a721284ff21832b44fb6e7d2bed22a536850
梦泽 陈 <1398947448@qq.com>
2024-11-03 23:51:06 +0800

    tested perf

--- src/services/record.py
+++ src/services/record.py
@@ -4,6 +4,11 @@
 import csv
 from task_timer import *
 
+def dur_reg(duration:timedelta)->timedelta: # 去除天数和微秒数
+    return timedelta(seconds=duration.seconds)
+def dtm_reg(dt:datetime)->datetime:
+    return dt.replace(microsecond=0)
+dt_fmt = "%Y-%m-%d %H:%M:%S"
 TaskRecord=namedtuple('TaskRecord',('task_id','start','end','valid_time','note'))
 class TaskRecorder:
     file_path = 'data/task_rec.csv'
@@ -16,7 +21,7 @@ class TaskRecorder:
             raise ValueError('Timer not ended yet')
         with open(cls.file_path,'a',newline='')as f:
             writer = csv.writer(f)
-            writer.writerow([input.task_id,input.start,input.end,input.used_time,note])
+            writer.writerow([input.task_id,dtm_reg(input.start),dtm_reg(input.end),dur_reg(input.used_time),note])
 
     @classmethod
     def add_counter_rec(cls,task_id:int,time:datetime=None,note:str=None)->None:
@@ -26,7 +31,7 @@ class TaskRecorder:
             time = datetime.now()
         with open(cls.file_path,'a',newline='')as f:
             writer = csv.writer(f)
-            writer.writerow([task_id,time,time,None,note])
+            writer.writerow([task_id,dtm_reg(time),dtm_reg(time),None,note])
     
     @classmethod
     def query(cls,task_id:int=None,start_range:tuple[datetime]=None,end_range:tuple[datetime]=None)->list[TaskRecord]:
@@ -36,17 +41,21 @@ class TaskRecorder:
         if task_id is not None:
             records = [record for record in records if int(record.task_id)==int(task_id)]
         if start_range is not None:
-            records= [record for record in records if datetime(record.start)>=start_range[0] and datetime(record.start)<=start_range[1]]
+            records= [record for record in records if datetime.strptime(record.start,dt_fmt)>=start_range[0] and datetime.strptime(record.start,dt_fmt)<=start_range[1]]
         if end_range is not None:
-            records= [record for record in records if datetime(record.end)>=end_range[0] and datetime(record.end)<=end_range[1]]
+            records= [record for record in records if datetime.strptime(record.end,dt_fmt)>=end_range[0] and datetime.strptime(record.end,dt_fmt)<=end_range[1]]
         return records
     @classmethod
     def delete(cls,task_id:int,start:datetime,end:datetime)->Optional[TaskRecord]:
-        pass
+        pass # TODO
         
 
 if __name__ == '__main__':
-    TaskRecorder.add_timer_rec(TimerStatus(1,timedelta(10.24423),datetime.now(),datetime.now()+timedelta(10.872789),False,timedelta(seconds=10e5)),note='test')
-    
+    # import random
+    # VARY = 1e5
+    # while True:
+    #     TaskRecorder.add_timer_rec(TimerStatus(1,timedelta(VARY*random.random()),datetime.now()-timedelta(VARY*random.random()),datetime.now()+timedelta(VARY*random.random()),False,timedelta(seconds=VARY*random.random())),note='test')
+    #     TaskRecorder.add_counter_rec(10,datetime.now()+timedelta(random.uniform(-VARY,+VARY)))
     
     breakpoint()
+    # TaskRecorder.query(task_id=1,start_range=(datetime.now()-timedelta(RNG),datetime.now()+timedelta(RNG)),end_range=(datetime.now()-timedelta(RNG),datetime.now()+timedelta(RNG)))
